d13a5e1a5bf4f307967ee7a4ec2aafaa4c07c465
sudhirgaurav <sudhirgauravec@gmail.com>
2024-04-25 12:43:54 +0530

    Kafka Synchronous call

--- product_microservice/src/main/java/com/sudhirindustries/ws/product_microservice/service/ProductServiceImp.java
+++ product_microservice/src/main/java/com/sudhirindustries/ws/product_microservice/service/ProductServiceImp.java
@@ -2,10 +2,12 @@ package com.sudhirindustries.ws.product_microservice.service;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import com.sudhirindustries.ws.product_microservice.controller.CreateProductModel;
 
@@ -26,7 +28,11 @@ public class ProductServiceImp implements ProductService {
 		
 		//TODO: persist the data into db table before pushing an event 
 		
+		//Creating product
 		ProductCreatedEvent createdEvent = new ProductCreatedEvent(productid, product.getTitle(),product.getPrice(),product.getQuantity());
+		
+		//*************************************Asynchronous call*****************************************
+		/*
 		CompletableFuture<org.springframework.kafka.support.SendResult<String, ProductCreatedEvent>> completableFuture=	kafkaTemplate.send("product_create_event_topic", productid, createdEvent);// This ll send msg asynchronously . Means Producer will not wait for the response from the kafka broker 
 		completableFuture.whenComplete((result, exception) ->{
 			
@@ -38,6 +44,25 @@ public class ProductServiceImp implements ProductService {
 			
 		} );
 		completableFuture.join(); // this line will make above call to synchronous [IMp]
+		*/
+		//*************************************************************************************
+	
+		
+		//********************************Synchronous call *************************************
+		SendResult<String, ProductCreatedEvent> result = null;
+		try {
+			 result=	kafkaTemplate.send("product_create_event_topic", productid, createdEvent).get();
+		} catch (InterruptedException | ExecutionException e) {
+			logger.error("Publishng message to kafka topic failed ....");
+			e.printStackTrace();
+		}
+
+		int partition =result.getRecordMetadata().partition();
+		String topic = result.getRecordMetadata().topic();
+		Long offset =result.getRecordMetadata().offset();
+		logger.info("Event sent to kafka topic : "+ topic+ " partition : "+partition + "offset : "+offset);
+		
+		//*****************************************************************************************
 		logger.info("Returning productid:  : "+productid);
 
 		return productid;
